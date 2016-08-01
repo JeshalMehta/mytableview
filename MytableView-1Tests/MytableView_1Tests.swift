d66fc468091e10d085ec55963d7debb8259c9454
Jeshal Mehta <jeshalmehta@jeshals-macbook-pro.local>
2016-08-01 08:13:08 +0530

    Initial Commit

---
+++ MytableView-1Tests/MytableView_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MytableView_1Tests.swift
+//  MytableView-1Tests
+//
+//  Created by Jeshal Mehta on 01/08/16.
+//  Copyright © 2016 Jeshal Mehta. All rights reserved.
+//
+
+import XCTest
+@testable import MytableView_1
+
+class MytableView_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
